5891fc897c3d2fa19df64ecbdee079d1d1cd8877
Abdeljebar <abdeljebarhinda@gmail.com>
2023-12-18 10:36:50 +0100

    add new client

--- src/main/java/com/clients/demo/services/ClientService.java
+++ src/main/java/com/clients/demo/services/ClientService.java
@@ -16,7 +16,7 @@ public class ClientService {
         clients.add(new Client(1L , "Jabran" , "Maroc")) ;
         clients.add(new Client(2L , "Mohsin" , "Tunis")) ;
         clients.add(new Client(3L , "Omar" , "Maroc")) ;
-        //clients.add(new Client(4L , "Rachid" , "Egypt")) ;
+        clients.add(new Client(4L , "Rachid" , "Egypt")) ;
        //clients.add(new Client(5L , "Mohamed" , "Maroc")) ;
 
         return clients ;
